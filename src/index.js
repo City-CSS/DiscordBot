26075744d764aa947d8f3aa417603eb004ed2dbf
JustFirefly <siddiqui.m.312@gmail.com>
2023-10-19 23:08:10 +0100

    working webscrapper
    +added puppeteer libraries

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {Client, IntentsBitField,} = require("discord.js");
+const {Client, IntentsBitField, Partials} = require("discord.js");
 const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
 //create an interactable bot
@@ -11,7 +11,12 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
         IntentsBitField.Flags.GuildVoiceStates,
+        IntentsBitField.Flags.MessageContent,
     ],
+    partials: [
+        Partials.Channel,
+        Partials.Message
+      ],
 });
 
 (() => {
